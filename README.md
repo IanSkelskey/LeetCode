a0f34b8f9e15aa437ca084be62340ec20526ea18
Ian Skelskey <ianskelskey@gmail.com>
2022-07-20 23:58:16 -0400

    Upload integer to roman solution

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
+![leetlogo.svg](./assets/leet-logo.svg)
+
 # Leet Code
 
 Solutions to LeetCode problems written in Java
 
 ## Complete Solutions
 
+12. Integer to Roman
 13. Roman to Integer
--- RomanToInteger.java
+++ RomanToInteger.java
@@ -3,66 +3,35 @@ import java.util.HashMap;
 class RomanToInteger {
 
     private final HashMap<Character, Integer> romanValueMap = newRomanMap();
-
-    private char currentRoman;
-    private char previousRoman;
-
     private int currentRomanValue;
-    private int previousRomanValue;
+    private int nextRomanValue;
 
     public int romanToInt(String s) {
-
         int output = 0;
-
         if (s.length() == 1) {
-            setCurrentRoman(s.charAt(0));
-            setCurrentRomanValue(currentRoman);
+            setCurrentRomanValue(s.charAt(0));
             return currentRomanValue;
         }
-
-        for (int i = 1; i < s.length(); i++) {
-
-            setCurrentRoman(s.charAt(i));
-            setPreviousRoman(s.charAt(i-1));
-
-            setCurrentRomanValue(currentRoman);
-            setPreviousRomanValue(previousRoman);
-
-            if (this.currentRomanValue > this.previousRomanValue) {
-                output += (this.currentRomanValue - this.previousRomanValue);
-                i++;
-                if (i == s.length() - 1) {
-                    setCurrentRoman(s.charAt(i));
-                    setCurrentRomanValue(currentRoman);
-                    output += this.currentRomanValue;
-                }
-            } else if (i < s.length() - 1) {
-                output += this.previousRomanValue;
-            }  else {
-                output += this.previousRomanValue;
-                output += this.currentRomanValue;
+        for (int i = 0; i < s.length(); i++) {
+            setCurrentRomanValue(s.charAt(i));
+            if (i != s.length() - 1) {
+                setNextRomanValue(s.charAt(i+1));
             }
-
+            if (this.currentRomanValue < this.nextRomanValue) {
+                output -= currentRomanValue;
+            } else {
+                output += currentRomanValue;
+            }
         }
-
         return output;
-
-    }
-
-    private void setCurrentRoman(char c) {
-        this.currentRoman = c;
     }
 
     private void setCurrentRomanValue(char c) {
         this.currentRomanValue = this.romanValueMap.get(c);
     }
 
-    private void setPreviousRoman(char c) {
-        this.previousRoman = c;
-    }
-
-    private void setPreviousRomanValue(char c) {
-        this.previousRomanValue = this.romanValueMap.get(c);
+    private void setNextRomanValue(char c) {
+        this.nextRomanValue = this.romanValueMap.get(c);
     }
 
     private HashMap<Character, Integer> newRomanMap() {
